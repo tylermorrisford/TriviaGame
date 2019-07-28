0dc186f5177f7ca2c19340c0210193cad5fb1e45
tylermorrisford <tylerford@gmail.com>
2019-07-28 14:56:48 -0600

    built wireframe, added minimal css, began to pseudocode js and jQuery

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // create necessary variables
+timer = 0;
+questionNumber = 0;
+playing = false;
+// questions as objects, then all objects in an array (for randomized questions?)
+
+// when users presses a key, start game
+$(document).keyup(function() {
+// change #question to show question1
+// set timer to 20 * 1000
+// show 3 incorrect answers and one correct answer
+});
+
+
+
+
+
+
+
+
+
+})
