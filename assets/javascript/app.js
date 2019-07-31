3b7c68545b63f575c771f04f01f0cc36b0c3da3b
tylermorrisford <tylerford@gmail.com>
2019-07-30 22:59:57 -0600

    fixed boolean flag logic for starting game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     // create necessary variables
 var timer = 0;
-var questionNumber = 0;
-var playing = false;
-// questions as objects, then all objects in an array (for randomized questions?)
+var questionNumber = 0; 
+var clockRunning = false;
+// var intervalId;
+var correct = 0;
+var incorrect = 0;
+// questions as objects, then all objects in an array
 var questions = [
     {
     question: "The largest recorded earthquake in the US occurred where?",
@@ -22,12 +25,15 @@ var questions = [
     }
 
 ]
+   
 // when users presses a key, start game
-$(document).keyup(function() {
+$(document).keypress(function() {
 // set playing to true
-playing = true;
+if (!clockRunning) {
+clockRunning = true;
 // change title to 'let's play!'
 $("#title").text("Let's Play!");
+
 // nextQuestion() function:-------
 // nextQuestion();
 // change #question to show question1
@@ -35,7 +41,14 @@ $("#question").text(questions[questionNumber].question);
 // questionNumber++;
 // set timer to 15 * 1000 -- setInterval for counting down, clearInterval if click
 timer = 15;
-$("#timer").text("Time Remaining: " + timer);
+var countdown = setInterval(function(){
+    $("#timer").text("Time Remaining: " + timer);
+    timer -= 1;
+    if(timer <= 0){
+      clearInterval(countdown);
+      $("#timer").text("Time's Up!");
+    }
+}, 1000); 
 // setTimeout() CHECK OUT 'stopwatch.js' for clues
 // show 3 incorrect answers and one correct answer - write these into the objects
 // how to dynamically generate buttons... in object, answers are in an array, one object key 'correctAnswer', is answers[i];
@@ -43,10 +56,11 @@ $("#timer").text("Time Remaining: " + timer);
 for ( i = 0; i < 4; i++) {
     $("#game").append("<p><button class='btn'>" + questions[questionNumber].answers[i] + "</button></p>");
 };
+}
 });
 
-// CLICK EVENTS ---- checkAnswer()
 // if timer = 0 borrow red background flash from simon game; setTimeout 3 * 1000 nextQuestion()
+// CLICK EVENTS ---- checkAnswer()
 // if user clicks incorrect answer button, show correct answer; setTimeout 3 * 1000 nextQuestion()
 // if user clicks correct answer button, show great work; setTimeout 3 * 1000 nextQuestion()
 // increment correct or incorrect
